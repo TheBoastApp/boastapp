1687ffc01ae87b5163b291231d44b17b55ec99ed
Hannah Ryan <hjryan@uw.edu>
2023-01-30 20:04:49 -0800

    changed data format of props

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,30 @@ import React from "react";
 import "./index.css";
 import User from "./components/User";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [role, setRole] = useState("bird");
+  const [users, setUsers] = useState([
+    {
+      name: "Hannah",
+      role: "Never had a job",
+      image:
+        "https://media.licdn.com/dms/image/D5603AQEnbNh-48HU6Q/profile-displayphoto-shrink_400_400/0/1672980786363?e=1680739200&v=beta&t=94FLpb3vXQKHTidgVJWrMN-nh4qH7ysgvUE5jLBurvg",
+    },
+    {
+      name: "Kat",
+      role: "Software Engineer",
+      image:
+        "https://media.licdn.com/dms/image/D5603AQEjcre84lMKFQ/profile-displayphoto-shrink_400_400/0/1663706177584?e=1680739200&v=beta&t=zfGJNvfJfW3J771M99OP_KFzXGZpsyH1iIf4HFGhHk0",
+    },
+    {
+      name: "Baby",
+      role: "bird",
+      image:
+        "https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?",
+    },
+  ]);
   const showUsers = true;
   return (
     <div className="App">
@@ -17,20 +38,17 @@ function App() {
             }}
           ></input>
           <div className="flex flex-wrap justify-center">
-            <User
-              name="Hannah"
-              image="https://media.licdn.com/dms/image/D5603AQEnbNh-48HU6Q/profile-displayphoto-shrink_400_400/0/1672980786363?e=1680739200&v=beta&t=94FLpb3vXQKHTidgVJWrMN-nh4qH7ysgvUE5jLBurvg"
-            />
-            <User
-              name="Kat"
-              role="Software Engineer"
-              image="https://media.licdn.com/dms/image/D5603AQEjcre84lMKFQ/profile-displayphoto-shrink_400_400/0/1663706177584?e=1680739200&v=beta&t=zfGJNvfJfW3J771M99OP_KFzXGZpsyH1iIf4HFGhHk0"
-            />
-            <User
-              name="Baby"
-              role={role}
-              image="https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&w=800"
-            />
+            {users.map((user) => {
+              console.log(user);
+              return (
+                <User
+                  key={uuidv4()}
+                  name={user.name}
+                  role={user.role}
+                  image={user.image}
+                />
+              );
+            })}
           </div>
         </>
       ) : (
