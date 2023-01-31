06d855a769503a27d9b81ebd9b708bfa16679dff
Hannah Ryan <hjryan@uw.edu>
2023-01-30 19:19:09 -0800

    added tailwinds css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
 import React from "react";
-import "./App.css";
+import "./index.css";
 import User from "./components/User";
+import { useState } from "react";
 
 function App() {
-  {
-    console.log("hi kat");
-  }
+  const [role, setRole] = useState("bird");
   const showUsers = true;
   return (
     <div className="App">
       {showUsers ? (
         <>
-          <User name="Hannah" />
-          <User name="Kat" role="Software Engineer" />
+          <input
+            type="text"
+            onChange={(e) => {
+              setRole(e.target.value);
+            }}
+          ></input>
+          <div className="flex flex-wrap justify-center">
+            <User
+              name="Hannah"
+              image="https://media.licdn.com/dms/image/D5603AQEnbNh-48HU6Q/profile-displayphoto-shrink_400_400/0/1672980786363?e=1680739200&v=beta&t=94FLpb3vXQKHTidgVJWrMN-nh4qH7ysgvUE5jLBurvg"
+            />
+            <User
+              name="Kat"
+              role="Software Engineer"
+              image="https://media.licdn.com/dms/image/D5603AQEjcre84lMKFQ/profile-displayphoto-shrink_400_400/0/1663706177584?e=1680739200&v=beta&t=zfGJNvfJfW3J771M99OP_KFzXGZpsyH1iIf4HFGhHk0"
+            />
+            <User
+              name="Baby"
+              role={role}
+              image="https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&w=800"
+            />
+          </div>
         </>
       ) : (
         <p>you can't see the user</p>
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,9 +1,31 @@
-function User({ name, role }: { name: string; role?: string }) {
+function User({
+  name,
+  role,
+  image,
+}: {
+  name: string;
+  role?: string;
+  image?: string;
+}) {
   return (
-    <>
-      <h3>User: {name}</h3>
-      <h5>{role ? role : "Never had a job"}</h5>
-    </>
+    <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img
+        className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+        src={image}
+        alt="headshot"
+      />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">{name}</p>
+          <p className="text-slate-500 font-medium">
+            {role ? role : "Never had a job"}
+          </p>
+        </div>
+        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+          Message
+        </button>
+      </div>
+    </div>
   );
 }
 
