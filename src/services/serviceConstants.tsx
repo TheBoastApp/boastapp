3815278597cd4cc22d5b18cab25dfca31271fc4b
Kat <katckime@gmail.com>
2023-03-16 00:14:32 -0700

    Changing base URL back to vercel

--- src/services/serviceConstants.tsx
+++ src/services/serviceConstants.tsx
@@ -1,3 +1,4 @@
-const BASE_URL = 'http://localhost:3001/users';
+// const BASE_URL = 'http://localhost:3001/users';
+const BASE_URL = 'https://boast-play-db.vercel.app/users'
 
 export default { BASE_URL };
